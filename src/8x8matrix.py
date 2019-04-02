1767dc8e8d7d6686f732a952a23d9b2996f2b9e2
Nicholas Hallman <ngahallman@gmail.com>
2019-04-02 12:01:38 -0400

    created basic test

---
+++ src/8x8matrix.py
@@ -0,0 +1,25 @@
+from luma.core import legacy
+from luma.core.legacy.font import proportional, CP437_FONT, LCD_FONT
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.led_matrix.device import max7219
+from PIL import Image
+
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial)
+
+def set_matrix(number):
+
+    number = str(number)
+
+    with canvas(device) as draw:
+        legacy.text(draw, (0,0), "Your string here", fill="white", 
+        font=proportional(CP437_FONT) )
+    
+    return None
+
+try:
+    while True:
+        set_matrix(3)
+except:
+    pass
