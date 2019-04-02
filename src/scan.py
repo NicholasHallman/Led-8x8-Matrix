dbef1f0f474bc4b3deb617ee93c52a04822b961b
Nicholas Hallman <ngahallman@gmail.com>
2019-04-02 12:47:31 -0400

    scan finished

--- src/scan.py
+++ src/scan.py
@@ -3,4 +3,21 @@ from luma.core.render import canvas
 from luma.led_matrix.device import max7219
 from PIL import Image
 
+import time
+
+INTERVAL = 1000/60
+
 im = Image.new(1,(8,8), color=0 )
+pixels = im.load()
+
+row = 0
+
+while True:
+    with canvas(max7219) as draw:
+        draw.line([(row,0),(row,7)],None)
+        row += 1
+        if(row == 8):
+            row = 0
+        time.sleep(INTERVAL)
+
+    
