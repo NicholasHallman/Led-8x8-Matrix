cf53f709454a86c8027696df8daf02199eb56f78
Nicholas Hallman <ngahallman@gmail.com>
2019-04-04 19:09:26 -0400

    added temios input

--- src/snake.py
+++ src/snake.py
@@ -3,7 +3,9 @@ from luma.core.render import canvas
 from luma.led_matrix.device import max7219
 from PIL import Image
 from copy import deepcopy
-from pynput.keyboard import Key, Listener
+
+from kbhit import KBHit
+
 import time
 import random
 import sys
@@ -11,28 +13,15 @@ import sys
 INTERVAL = 1/10
 
 serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial)    
+device = max7219(serial)  
+
+kb = KBHit()
 
 snake = []
 direction = 0 # 0 up, 1 right, 2 down, 3 left
 apple = (0,0)
 released = True
 
-def onPress(key):
-    if released:
-        if key == Key.up:
-            direction = 0
-        elif key == Key.right:
-            direction = 1
-        elif key == Key.down:
-            direction = 2
-        elif key == Key.left:
-            direction = 3
-        release = False
-
-
-def onRelease(key):
-    released = True
 
 def draw():
     with canvas(device) as draw:
@@ -40,6 +29,9 @@ def draw():
         draw.point(apple,fill=128)
 
 def moveSnake():
+    kb.kbhit()
+    direction = kb.getarrow()
+
     last = (-1,-1)    
     if direction == 0:
         for point in snake:
@@ -92,9 +84,6 @@ def checkRules():
         snake.append(deepcopy(snake[-1]))
 
 def gameLoop():
-    with Listener(on_press=onPress, on_release=onRelease) as listener:
-        listener.join()
-    
     while True:
         moveSnake()
         checkRules()
